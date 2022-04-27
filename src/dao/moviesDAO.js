fe126b219490f849c186c258e620d3161cc1edea
Tony Li <weijia.tony.li@gmail.com>
2022-04-27 17:25:17 -0400

    Ticket: Handling Errors

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -334,7 +334,7 @@ export default class MoviesDAO {
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
-      throw e
+      return null;
     }
   }
 }
